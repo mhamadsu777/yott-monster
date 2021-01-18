f9bf8b0321a1ddf865fd2b153e88e45b4398b2e4
mhamadsu777 <66016885+mhamadsu777@users.noreply.github.com>
2021-01-18 16:14:42 +0200

    Update __main__.py

--- scdlbot/__main__.py
+++ scdlbot/__main__.py
@@ -17,7 +17,7 @@ console_handler.setFormatter(console_formatter)
 console_handler.setLevel(logging.DEBUG)
 logging_handlers.append(console_handler)
 
-tg_bot_token = os.environ['TG_BOT_TOKEN']
+tg_bot_token = os.getenv['TG_BOT_TOKEN']
 alert_chat_ids = list(map(int, os.getenv('ALERT_CHAT_IDS', '0').split(',')))
 telegram_handler = TelegramHandler(token=tg_bot_token, chat_id=str(alert_chat_ids[0]))
 telegram_handler.setLevel(logging.WARNING)
